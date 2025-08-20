80cd76077764db3a79cc09c46054019af56815fb
pietro-celati <p.celati@studenti.unipi.it>
2025-08-20 11:38:48 +0200

    Create keccak-tiny.h

---
+++ JMT/include/keccak-tiny.h
@@ -0,0 +1,21 @@
+#ifndef KECCAK_FIPS202_H
+#define KECCAK_FIPS202_H
+#define __STDC_WANT_LIB_EXT1__ 1
+#include <stdint.h>
+#include <stdlib.h>
+
+int keccak_256(uint8_t* out, const uint8_t* in, size_t inlen);
+
+#define decshake(bits) \
+  int shake##bits(uint8_t*, size_t, const uint8_t*, size_t);
+
+#define decsha3(bits) \
+  int sha3_##bits(uint8_t*, size_t, const uint8_t*, size_t);
+
+decshake(128)
+decshake(256)
+decsha3(224)
+decsha3(256)
+decsha3(384)
+decsha3(512)
+#endif
